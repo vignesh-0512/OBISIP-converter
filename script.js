33c39f8eac0682e701fa8c6de22e95b1a21abfca
vignesh-0512 <115525353+vignesh-0512@users.noreply.github.com>
2023-08-14 20:14:36 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+let celsius = document.getElementById('celsius');
+
+ function celToFar(){
+    let output = ((parseFloat(celsius.value)*9/5)+32);
+    fahrenheit.value = parseFloat(output.toFixed(2));
+ }
+ 
+
+
+
+
+
+
+
+
+
